73a3dd42212efa00828944d314fbad42072205d2
woojae-kim <138834162+woojae-kim@users.noreply.github.com>
2023-07-10 12:59:44 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+//const image1 = document.getElementById("img1");
+/*const image2 = document.getElementById("img2");
+const image3 = document.getElementById("img3");
+const image4 = document.getElementById("img4");
+const image5 = document.getElementById("img5"); 
+
+const imgNum = [0, 1, 2, 3, 4];
+
+for (let i = 0; i < 6; i++) {
+  console.log(i);
+}
+
+
+function imageSwitch(id, visible){
+  img.style.visibility = (visible ? 'visible' : 'hidden');
+} */
