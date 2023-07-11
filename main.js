d9f3502b6fc61bfe7ceeff8e285e05374c6b842a
woojae-kim <138834162+woojae-kim@users.noreply.github.com>
2023-07-11 10:18:49 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-//const image1 = document.getElementById("img1");
-/*const image2 = document.getElementById("img2");
+//create a variable for each image
+const image1 = document.getElementById("img1");
+const image2 = document.getElementById("img2");
 const image3 = document.getElementById("img3");
 const image4 = document.getElementById("img4");
 const image5 = document.getElementById("img5"); 
 
-const imgNum = [0, 1, 2, 3, 4];
+//create an array of the images
+const imgNum = [image1, image2, image3, image4, image5];
 
-for (let i = 0; i < 6; i++) {
-  console.log(i);
+
+//get a random number between 0 and max
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
+//create a variable for a random integer
+const randInt = getRandomInt(5);
 
-function imageSwitch(id, visible){
-  img.style.visibility = (visible ? 'visible' : 'hidden');
-} */
+console.log(randInt)
+
+//hide a photo in the array based on the random integer
+ imgNum[randInt].style.display = 'none';
