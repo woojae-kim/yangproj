c70fde7515fdb782d1b43dce8c3445b6e8e39663
woojae-kim <138834162+woojae-kim@users.noreply.github.com>
2023-07-11 10:45:05 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,16 +8,22 @@ const image5 = document.getElementById("img5");
 //create an array of the images
 const imgNum = [image1, image2, image3, image4, image5];
 
-
-//get a random number between 0 and max
+//function for getting a random number between 0 and max
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-//create a variable for a random integer
-const randInt = getRandomInt(5);
+setInterval(() => {
+//first hide all the images
+for (let i = 0; i < imgNum.length; i++) {
+  imgNum[i].style.display = 'none';
+}
 
-console.log(randInt)
+//infinitley generate a new number and show a photo
 
-//hide a photo in the array based on the random integer
- imgNum[randInt].style.display = 'none';
+  let randInt = getRandomInt(imgNum.length);
+  
+  //hide a photo in the array based on the random integer
+  imgNum[randInt].style.display = 'inline';
+}, 1000);
+
